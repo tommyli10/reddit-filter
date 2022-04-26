282c651d62a6746cc070c517d4705c30c4b23176
Tommy Li <tommyli76278@gmail.com>
2022-04-25 23:48:25 -0700

    added remove by tag function

--- Content.js
+++ Content.js
@@ -1,17 +1,3 @@
-// class = 'rpBJOHq2PR60pnwJlUyP0'
-// let filter = 'sonic';
-
-// const list = document.querySelector('.rpBJOHq2PR60pnwJlUyP0');
-
-// const titles = list.getElementsByTagName('h3');
-
-// Array.from(titles).forEach(post => {
-//     if (post.textContent.toLocaleLowerCase().includes(filter)) {
-//         const parent = post.closest('.rpBJOHq2PR60pnwJlUyP0 > div');
-//         parent.parentNode.removeChild(parent);
-//     }
-// });
-
 console.log("script running");
 
 chrome.runtime.onMessage.addListener(gotMessage);
@@ -21,13 +7,23 @@ function gotMessage(message, sender, sendResponse) {
 
     let filter = message.txt.toLowerCase();
 
+    // selecting all reddit post, which are all under a div with the class name of '.rpBJOHq2PR60pnwJlUyP0'
     const list = document.querySelector('.rpBJOHq2PR60pnwJlUyP0');
-
     const titles = list.getElementsByTagName('h3');
+    const tags = list.getElementsByTagName('span');
 
+    // get rid of all post with filter word in title
     Array.from(titles).forEach(post => {
         if (post.textContent.toLocaleLowerCase().includes(filter)) {
             const parent = post.closest('.rpBJOHq2PR60pnwJlUyP0 > div');
+            parent.parentNode.removeChild(parent);
+        }
+    });
+
+    // get rid of all post with filter word in tags
+    Array.from(tags).forEach(tag => {
+        if (tag.textContent.toLocaleLowerCase().includes(filter)) {
+            const parent = tag.closest('.rpBJOHq2PR60pnwJlUyP0 > div');
             parent.parentNode.removeChild(parent);
         }
     });
